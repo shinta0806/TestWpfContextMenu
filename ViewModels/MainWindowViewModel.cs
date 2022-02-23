5ead755df9b15640fc2d21bfdab7d074172613df
shinta0806 <shinta.0806@gmail.com>
2022-02-23 12:40:39 +0900

    シンプルなコンテキストメニューを実装

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,70 @@
-﻿using Livet;
+﻿// ============================================================================
+// 
+// メインウィンドウの ViewModel
+// 
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// 
+// ----------------------------------------------------------------------------
+
+using Livet;
 using Livet.Commands;
-using Livet.EventListeners;
-using Livet.Messaging;
-using Livet.Messaging.IO;
-using Livet.Messaging.Windows;
+
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using TestWpfContextMenu.Models;
 
 namespace TestWpfContextMenu.ViewModels
 {
-	public class MainWindowViewModel : ViewModel
+	internal class MainWindowViewModel : ViewModel
 	{
-		// Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+		// ====================================================================
+		// public プロパティー
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// View 通信用のプロパティー
+		// --------------------------------------------------------------------
+
+		// ステータスバーに表示するメッセージ
+		private String _statusBarMessage = String.Empty;
+		public String StatusBarMessage
+		{
+			get => _statusBarMessage;
+			set => RaisePropertyChangedIfSet(ref _statusBarMessage, value);
+		}
+
+		// --------------------------------------------------------------------
+		// コマンド
+		// --------------------------------------------------------------------
+
+		#region メニューアイテムの制御
+		private ListenerCommand<String>? _menuItemTestClickedCommand;
+
+		public ListenerCommand<String> MenuItemTestClickedCommand
+		{
+			get
+			{
+				if (_menuItemTestClickedCommand == null)
+				{
+					_menuItemTestClickedCommand = new ListenerCommand<String>(MenuItemTestClicked);
+				}
+				return _menuItemTestClickedCommand;
+			}
+		}
+
+		public void MenuItemTestClicked(String parameter)
+		{
+			StatusBarMessage = DateTime.Now.ToString("HH:mm:ss") + "　" + parameter;
+		}
+		#endregion
+
+		// ====================================================================
+		// public 関数
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// 初期化
+		// --------------------------------------------------------------------
 		public void Initialize()
 		{
 		}
